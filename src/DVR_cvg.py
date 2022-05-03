Units fixed

src/DVR_cvg.py
@@ -21,20 +21,24 @@ def N_convergence(N, R, avg=1, dim=3, level=1):
     dim_factor = np.zeros(3, dtype=int)
     dim_factor[:dim] = 1
     R = R * dim_factor
-    x = np.linspace(-1.1 * R[0], 1.1 * R[0], int(1000))[:, None]
+    x = np.linspace(-1.1 * R[0], 1.1 * R[0], int(1000))
+    y = np.array([0])
+    z = np.array([0])
     p = []
 
     for i in N:
         n = i * np.array([1, 1, 1])
         dx = R / n
         n = n * dim_factor
-        D = DVR(n, R, sparse=True)
+        D = DVR(n, R, avg=avg, sparse=True)
         print('dx= {}w'.format(dx))
         print('R= {}w'.format(R))
         print('n=', n)
         V, W = H_solver(D)
         E = np.append(E, V[:k].reshape(1, -1), axis=0)
-        p.append(psi(n, dx[0], W, x)[:, :level])
+        p.append(
+            psi(n, dx, W.reshape(*(D.n + 1 - D.init), k), x, y, z)[:, 0,
+                                                                   0, :level])
     dE = np.diff(E, axis=0)
 
     return np.array(N), dE, E, x / R[0], p

src/DVR_exe.py
@@ -28,7 +28,7 @@ def eigen_list(dvr: dynamics):
     W_list = [W1, W2]
     print(
         'n={}, dx={}, p={}, model={}, t={} stroboscopic states preparation finished.'
-        .format(dvr.n[:dvr.dim], dvr.dx[:dvr.dim], dvr.p[:dvr.dim], dvr.model,
+        .format(dvr.n[dvr.nd], dvr.dx[dvr.nd], dvr.p[dvr.nd], dvr.model,
                 dvr.stop_time_list))
     print("eigen_list: eigenstates memory usage: {:.2f} MiB.".format(
         asizeof(W_list) / 2**20))
@@ -36,6 +36,7 @@ def eigen_list(dvr: dynamics):
 
 
 class Output:
+
     def __init__(self,
                  t=None,
                  gs=None,
@@ -99,12 +100,12 @@ def DVR_exe(dvr: dynamics) -> None:
 
     np.set_printoptions(precision=2, suppress=True)
     print("{}D N={} R0={}w\nfreq={}kHz\n{} potential starts.".format(
-        dvr.dim, dvr.N, dvr.R0[:dvr.dim], dvr.freq_list, dvr.model))
+        dvr.dim, dvr.N, dvr.R0[dvr.nd], dvr.freq_list, dvr.model))
 
     time0 = time()
 
     print("n={}, dx={}w, p={}, model={},\nt={},\nt_step={}\nstarts.".format(
-        dvr.n[:dvr.dim], dvr.dx[:dvr.dim], dvr.p[:dvr.dim], dvr.model,
+        dvr.n[dvr.nd], dvr.dx[dvr.nd], dvr.p[dvr.nd], dvr.model,
         dvr.stop_time_list, dvr.t_step_list))
     mem_est(dvr.n, dvr.p)
 
@@ -113,6 +114,7 @@ def DVR_exe(dvr: dynamics) -> None:
 
     dvr.avg = 1 / 2
     psi0 = dvr.init_state()
+    dvr.avg = 1
 
     time2 = time()
     print('Initial state preparation finished. Time spent: {:.2f}s.\n'.format(
@@ -137,7 +139,7 @@ def DVR_exe(dvr: dynamics) -> None:
     if dvr.absorber:
         print(
             'Absorption potential is enabled. Paramter: L={:.2f}w V_OI={:.2f}kHz\n'
-            .format(dvr.LI, dvr.VI * dvr.V0_SI / dvr.kHz_2p))
+            .format(dvr.LI, dvr.VI / dvr.kHz_2p))
 
     for fi in range(dvr.freq_list_len):  # frequency unit, V0 ~ 104.52kHz
         time4 = time()
@@ -185,8 +187,9 @@ def DVR_exe(dvr: dynamics) -> None:
             else:
                 U = one_period_evo(E_list, W_list, dvr.t1, dvr.t2, dvr,
                                    Winv_list)
+                # Delete when last frequency is calculated
                 if fi == dvr.freq_list_len - 1:
-                    del E_list, W_list, Winv_list  # Only enabled when one frequency is calculated
+                    del E_list, W_list, Winv_list
 
             print(cond_str + "time evolution operator prepared.")
             mem_check(limit=6)
@@ -244,9 +247,9 @@ def mem_check(limit=6):
     display_top(snapshot, limit=limit)
 
 
-def dynamics_period(dynamics: dynamics, t_step, cond_str, psi1, psi, time6, fn,
-                    W, n_period, E_power_n):
-    for t in np.arange(t_step, dynamics.stop_time + t_step, t_step):
+def dynamics_period(dvr: dynamics, t_step, cond_str, psi1, psi, time6, fn, W,
+                    n_period, E_power_n):
+    for t in np.arange(t_step, dvr.stop_time + t_step, t_step):
         psi = dynamics_by_period(n_period,
                                  E_power_n,
                                  W,
@@ -254,7 +257,7 @@ def dynamics_period(dynamics: dynamics, t_step, cond_str, psi1, psi, time6, fn,
                                  fixed_period=True)
 
         rho_gs = abs(psi1 @ psi)**2
-        if dynamics.wavefunc:
+        if dvr.wavefunc:
             psi_w = W @ psi
             rho_trap = la.norm(
                 psi_w
@@ -262,30 +265,29 @@ def dynamics_period(dynamics: dynamics, t_step, cond_str, psi1, psi, time6, fn,
         else:
             rho_trap, psi_w = None, None
 
-        io = Output(t, rho_gs, dynamics.wavefunc, (rho_trap, psi_w))
+        io = Output(t, rho_gs, dvr.wavefunc, (rho_trap, psi_w))
         io.write_to_file(fn)
 
-        dynamics.step_count += 1
+        dvr.step_count += 1
 
-        print_progress(cond_str, dynamics.step_count, time6)
+        print_progress(cond_str, dvr.step_count, time6)
 
 
-def dynamics_realtime(dynamics: dynamics, t_step, cond_str, psi, psi1, time6,
-                      fn, U0, U1):
-    for t_count in np.arange(t_step, dynamics.stop_time + t_step, t_step):
-        dynamics.step_count += 1
-        psi = wavepocket_dynamics(psi, U0, U1, dynamics.step_count,
-                                  dynamics.step_no)
+def dynamics_realtime(dvr: dynamics, t_step, cond_str, psi, psi1, time6, fn,
+                      U0, U1):
+    for t_count in np.arange(t_step, dvr.stop_time + t_step, t_step):
+        dvr.step_count += 1
+        psi = wavepocket_dynamics(psi, U0, U1, dvr.step_count, dvr.step_no)
 
-        t = t_count * dynamics.t_unit
+        t = t_count * dvr.t_unit
         rho_gs = abs(psi1 @ psi)**2
         rho_trap = la.norm(
             psi
         )**2  # Roughly estimate state proportion remaining within DVR space
-        io = Output(t, rho_gs, dynamics.wavefunc, (rho_trap, psi))
+        io = Output(t, rho_gs, dvr.wavefunc, (rho_trap, psi))
         io.write_to_file(fn)
 
-        print_progress(cond_str, dynamics.step_count, time6)
+        print_progress(cond_str, dvr.step_count, time6)
 
 
 def print_progress(cond_str, step_count, time6):

src/DVR_full.py
@@ -15,9 +15,9 @@ from time import time
 
 # Fundamental constants
 a0 = 5.29177E-11  # Bohr radius, in unit of meter
-Eha = 6579.68392E12 * 2 * np.pi  # Hartree energy, in unit of Hz
-amu = 1822.89  # atomic mass, in unit of electron mass
-hb = 1  # Reduced Planck const
+# Eha = 6579.68392E12 * 2 * np.pi  # Hartree energy, in unit of Hz
+amu = 1.66053907E-27  # atomic mass, in unit of kg
+h = 6.62607015E-34  # Planck constant
 # l = 780E-9 / a0  # 780nm, light wavelength
 
 dim = 3  # space dimension
@@ -63,7 +63,8 @@ class DVR:
             #     print(a[0])
             #     print(np.rint(self.LI / self.dx[self.nd]).astype(int))
             self.n[self.nd] += np.rint(self.LI / self.dx[self.nd]).astype(int)
-            print('DVR: n is set to {} by adding absorber.'.format(self.n))
+            print('DVR: n is set to {} by adding absorber.'.format(
+                self.n[self.nd]))
             self.R[self.nd] = self.n[self.nd] * self.dx[self.nd]
             print('DVR: R={}w is set.'.format(self.R[self.nd]))
 
@@ -115,17 +116,17 @@ class DVR:
 
         if model == 'Gaussian':
             # Experiment parameters in atomic units
+            self.hb = h / (2 * np.pi)  # Reduced Planck constant
             self.m = atom * amu  # Atom mass, in unit of electron mass
-            self.l = laser * 1E-9 / a0  # Laser wavelength, in unit of Bohr radius
-            self.kHz_2p = 2 * np.pi * 1E3  # Make in the frequency unit of 2 * pi * kHz
-            self.V0_SI = trap[
-                0] * self.kHz_2p * hb  # Input V0 is frequency in unit of kHz, convert to energy in unit of Joule
+            self.l = laser * 1E-9  # Laser wavelength, in unit of Bohr radius
+            self.kHz = 1E3  # Make in the frequency unit of kHz
+            self.kHz_2p = 2 * np.pi * 1E3  # Make in the agnular kHz frequency
+            self.V0 = trap[
+                0] * self.kHz_2p  # Input V0 is frequency in unit of kHz, convert to angular frequency 2 * pi * kHz
             self.w = trap[
-                1] * 1E-9 / a0  # Input in unit of nm, converted to Bohr radius
+                1] * 1E-9  # Input in unit of nm, converted to Bohr radius
 
-            self.V0 = self.V0_SI / Eha  # potential depth in unit of Hartree energy
-            # TO GET A REASONABLE ENERGY SCALE, WE SET v=1 AS THE ENERGY UNIT HEREAFTER
-
+            # TO GET A REASONABLE ENERGY SCALE, WE SET V0=1 AS THE ENERGY UNIT HEREAFTER
             self.mtV0 = self.m * self.V0
             if len(trap) < 3:
                 self.zR = np.pi * self.w / self.l  # ~4000nm, Rayleigh range, in unit of waist
@@ -133,27 +134,29 @@ class DVR:
                 self.zR = trap[2] / trap[1]  # Rayleigh range input by hand
 
             # self.Nmax = np.array([20, 20, 20])  # Max number of grid points
-            self.omega = np.sqrt(self.V0_SI / self.m) * np.array(
+            self.omega = np.sqrt(self.V0 / self.m) * np.array(
                 [np.sqrt(2) / self.w,
                  np.sqrt(2) / self.w, 1 / self.zR])  # Trap frequencies
-            self.hl = np.sqrt(hb /
+            self.hl = np.sqrt(self.hb /
                               (self.m * self.omega))  # Trap harmonic lengths
 
-            print("param_set: trap parameter V0={}kHz w={}m".format(
+            print("param_set: trap parameter V0={}kHz w={}nm".format(
                 trap[0], trap[1]))
         elif model == 'sho':
             # Harmonic parameters
+            self.hb = 1  # Reduced Planck constant
             self.omega = np.ones(dim)  # Harmonic frequencies
             self.m = 1.0
             self.w = 1.0
             self.mtV0 = self.m
-            self.V0_SI = 1.0
+            self.V0 = 1.0
             self.kHz = 1.0
             self.kHz_2p = 1.0
-            self.hl = np.sqrt(hb / (self.m * self.omega))  # Harmonic lengths
+            self.hl = np.sqrt(self.hb /
+                              (self.m * self.omega))  # Harmonic lengths
 
             print("param_set: trap parameter V0={} w={}".format(
-                self.V0_SI, self.w))
+                self.V0, self.w))
 
         self.R0 *= self.nd
         self.R *= self.nd
@@ -163,7 +166,8 @@ class DVR:
 
         ## Abosorbers
         if absorber:
-            self.VI *= self.kHz_2p / self.V0_SI  # Absorption potential strength in unit of V0
+            self.VI *= self.kHz_2p  # Absorption potential strength in unit of angular kHz frequency
+            self.VIdV0 = self.VI / self.V0  # Energy in unit of V0
 
     def Vfun(self, x, y, z):
         # Potential function
@@ -189,7 +193,7 @@ class DVR:
         #     print(L)
         Vi = np.sum(d / L, axis=3)
         if Vi.any() != 0.0:
-            V = -1j * self.VI * Vi
+            V = -1j * self.VIdV0 * Vi  # Energy in unit of V0
         return V
 
 
@@ -222,7 +226,8 @@ def Vmat(dvr: DVR):
     return V, no
 
 
-def psi(n, dx, W, x, y, z, p=np.zeros(dim, dtype=int)) -> np.ndarray:
+def psi(n, dx, W: np.ndarray, x, y, z, p=np.zeros(dim,
+                                                  dtype=int)) -> np.ndarray:
     init = get_init(n, p)
     # V = np.sum(
     #     W.reshape(*(np.append(n + 1 - init, -1))), axis=1
@@ -232,7 +237,11 @@ def psi(n, dx, W, x, y, z, p=np.zeros(dim, dtype=int)) -> np.ndarray:
     deltax[nd] = 1
     xn = [np.arange(init[i], n[i] + 1) for i in range(dim)]
     V = delta(p, [x / deltax[0], y / deltax[1], z / deltax[2]], xn)
-    psi = 1 / np.sqrt(np.prod(deltax)) * contract('il,jm,kn,lmn', *V, W)
+    # if W.ndim < 4:
+    #     W = W[..., *[None for i in range(4-W.ndim)]]
+    if W.ndim == 3:
+        W = W[..., None]
+    psi = 1 / np.sqrt(np.prod(deltax)) * contract('il,jm,kn,lmnp', *V, W)
     return psi
 
 
@@ -257,8 +266,11 @@ def kinetic_offdiag(T):
     return T
 
 
-def Tmat_1d(n, dx, mtV0, p=0):
+def Tmat_1d(dvr: DVR, i: int):
     # Kinetic energy matrix for 1-dim
+    n = dvr.n[i]
+    dx = dvr.dx[i] * dvr.w
+    p = dvr.p[i]
 
     init = get_init(np.array([n]), p)[0]
 
@@ -274,8 +286,8 @@ def Tmat_1d(n, dx, mtV0, p=0):
             T[:, 0] /= np.sqrt(2)
             T[0, :] /= np.sqrt(2)
             T[0, 0] = np.pi**2 / 3
-    # get kinetic energy in unit of V0, mtV0 = m * V0
-    T *= hb**2 / (2 * dx**2 * mtV0)
+    # get kinetic energy in unit of V0, hb is cancelled as V0 is in unit of angular freq, mtV0 = m * V0
+    T *= dvr.hb / (2 * dx**2 * dvr.mtV0)
     return T
 
 
@@ -288,8 +300,7 @@ def Tmat(dvr: DVR) -> np.ndarray:
     for i in range(dim):
         delta.append(np.eye(dvr.n[i] + 1 - dvr.init[i]))  # eg. delta_xx'
         if dvr.n[i]:
-            T0.append(Tmat_1d(dvr.n[i], dvr.dx[i] * dvr.w, dvr.mtV0,
-                              dvr.p[i]))  # append p-sector
+            T0.append(Tmat_1d(dvr, i))  # append p-sector
         # If the systems is set to have only 1 grid point (N = 0) in this direction, ie. no such dimension
         else:
             T0.append(None)

src/dynamics.py
@@ -22,7 +22,7 @@ class dynamics(DVR):
                  avg=1,
                  dim=3,
                  model='Gaussian',
-                 trap=(1.0452E2, 1000),
+                 trap=(104.52, 1000),
                  mem_eff=False,
                  wavefunc=False,
                  realtime=False,
@@ -55,7 +55,7 @@ class dynamics(DVR):
         self.freq_list_len = len(freq_list)
         self.step_no = time[0]
         self.stop_time_list = get_stop_time(freq_list, time[1],
-                                            self.V0_SI)  # Time in SI unit
+                                            self.V0)  # Time in SI unit
         self.freq_list = np.array(freq_list)
         self.dim = dim
         self.mem_eff = mem_eff
@@ -76,7 +76,7 @@ class dynamics(DVR):
     def init_state(self) -> np.ndarray:
         # Calculate GS of time-averaged potentiala
         print('init_state: initial state of T+%.1fV is calculated.' % self.avg)
-        ab = copy.deepcopy(self.absorber)
+        ab = copy.copy(self.absorber)
         self.absorber = False
         __, W = H_solver(self)
         psi = W[:, 0]
@@ -93,9 +93,9 @@ class dynamics(DVR):
         # ax = fig.add_subplot(1, len_freq_list, fi + 1)
 
         # time period of free system, in unit of s
-        self.t1 = 1 / (2 * 1E3 * self.freq)
+        self.t1 = 1 / (2 * self.kHz * self.freq)
         # time period of stroboscopic potential system, in unit of s
-        self.t2 = 1 / (2 * 1E3 * self.freq)
+        self.t2 = 1 / (2 * self.kHz * self.freq)
         self.T = self.t1 + self.t2
 
         # axs[1].plot(N_list, final_val)
@@ -132,7 +132,7 @@ class dynamics(DVR):
         sm_str = add_str(self.smooth, 'sm', (self.T0, self.Nslice))
         np.set_printoptions(precision=2, suppress=True)
         filename = '{} {} {:g} {:g} {:g} {:.2g} {:.2g} '.format(
-            self.n[:self.dim], self.dx[:self.dim], self.V0_SI / self.kHz_2p,
+            self.n[self.nd], self.dx[self.nd], self.V0 / self.kHz_2p,
             self.w, self.freq, self.stop_time, t_step)
         for str in (self.model, rt_str, sym_str, ab_str, sm_str):
             filename += str
@@ -151,14 +151,14 @@ def add_str(flag, label, param=None):
     return str
 
 
-def get_stop_time(freq_list: np.ndarray, t=0, V0_SI=0) -> np.ndarray:
+def get_stop_time(freq_list: np.ndarray, t=0, V0=0) -> np.ndarray:
     # NOTE: input freq_list must be in unit of kHz
     if t is 0:
         st = 4E-5 * np.exp(freq_list * 0.085)  # More accurate scaling
         # st = 2.5E-5 * np.exp(
         #     freq_list * 0.0954747)  # Legacy scaling to access 3D data
         # st[np.nonzero(freq_list < 39.4)] = 1E-3
-        # if V0_SI > 1.5E5 * 2 * np.pi:
+        # if V0 > 1.5E5 * 2 * np.pi:
         #     st *= 2
     else:
         if not isinstance(t, Iterable):
@@ -180,17 +180,12 @@ def copy_to_list(n, copy_time):
 
 def int_evo_ops(dvr: dynamics, E, W, t2, Winv=None):
     # interacting dynamics, calculate U by given eigensolution E, W of Hamiltonian
-    # NOTE: here energy is in unit of V0, ie. time is in unit of 1/V0, we need to multiply V0 to match unit of E
+    # NOTE: here energy is in unit of angular frequency, we need to multiply V0
     if t2 > 0:
         if dvr.absorber:
-            U1 = W @ (np.exp(-1j * E * dvr.V0_SI * t2 / hb)[:, None] * Winv)
-            # Pade approx., DEPRECATED as is much slower
-            # U1 = la.expm(-1j * W * t2 / hb)  # E left unused
-            # np.set_printoptions(threshold=sys.maxsize)
-            # print(-1j * W * t2 / hb)
+            U1 = W @ (np.exp(-1j * E * dvr.V0 * t2)[:, None] * Winv)
         else:
-            U1 = W @ (np.exp(-1j * E * dvr.V0_SI * t2 / hb)[:, None] *
-                      W.conj().T)
+            U1 = W @ (np.exp(-1j * E * dvr.V0 * t2)[:, None] * W.conj().T)
     else:
         N = W.shape[0]
         U1 = np.eye(N)
@@ -297,19 +292,9 @@ def one_period_evo_smooth(dvr: dynamics):
     U = np.eye(N)
     for i in range(dvr.Nslice):
         H = H_mat_w_f(dvr, ft[i])
-        # E, W = la.eig(H)
-        # U @= W @ (np.exp(-1j * E * dvr.V0_SI / hb)[:, None] * la.inv(W))
 
-        U = la.expm(-1j * dt * H * dvr.V0_SI / hb) @ U
-        # for j in range(i):
-        #     H += dt * dvr.V0_SI / 2 * (-1j * dt / hb) * commutator(
-        #         H_mat_w_f(dvr, ft[i]), H_mat_w_f(dvr, ft[j]))
-    # if __debug__:
-    #     print('norm H =', la.norm(H))
-    #     print('norm H0 =', la.norm(H0))
-    # H += H0
-    # E, W = la.eig(H)
-    # U = W @ (np.exp(-1j * E * dvr.V0_SI / hb)[:, None] * la.inv(W))
+        # H in unit of V0, ie. angular frequency
+        U = la.expm(-1j * dt * H * dvr.V0) @ U
     return U
 
 

src/plot_dynamics.py
@@ -39,7 +39,7 @@ class plot(dynamics):
                  dim=3,
                  smooth=(-1, 10),
                  model='Gaussian',
-                 trap=(1.0452E2, 1E-6),
+                 trap=(104.52, 1000),
                  mem_eff=False,
                  wavefunc=False,
                  realtime=False,
@@ -140,7 +140,8 @@ def plot_dynamics(N_list,
         fig = plt.figure(figsize=[6 * figno, 5 * dvr.freq_list_len])
         first_fig = True
         if dvr.freq_list_len == 1:
-            ax_list = [fig.subplots(dvr.freq_list_len, figno, sharey=True)]
+            ax_list = np.array(
+                [fig.subplots(dvr.freq_list_len, figno, sharey=True)])
         else:
             ax_list = fig.subplots(dvr.freq_list_len, figno, sharey=True)
     else:
@@ -235,7 +236,7 @@ def plot_dynamics(N_list,
             axs[2].legend()
             axs[2].set_xlabel(dvr.xlabel)
             axs[2].set_title(dvr.title2)
-        ax_list.append(axs)
+        ax_list = np.append(ax_list, axs)
     plt.savefig('{}d_{}.pdf'.format(dvr.dim, dvr.quantity))
     return fig
 
@@ -291,30 +292,27 @@ def plot_lifetime(N0_list,
         t_step = dvr.set_each_freq(fi)
 
         # NORMAL WAIST
-        dvr.VI *= dvr.V0_SI / dvr.kHz_2p
-        dvr.V0_SI = 104.52 * dvr.kHz_2p * hb  # 104.52kHz * h, potential depth, in SI unit, since hbar is set to 1 this should be multiplied by 2pi
-        dvr.w = 1E-6 / a0  # ~1000nm, waist length, in unit of Bohr radius
-        dvr.VI *= dvr.kHz_2p / dvr.V0_SI
+        dvr.V0 = 104.52 * dvr.kHz_2p  # 104.52kHz * 2 * pi, potential depth, in unit of angular freq
+        dvr.w = 1E-6  # ~1000nm, waist length, in unit of Bohr radius
+        dvr.VIdV0 = dvr.VI / dvr.V0  # Update VI in unit of V0
 
-        # TODO: CHECK IF R AND L VARYING WITH W CAUSES THE DIFFERENCE ON THE LIFETIME
+        # TODO: CHECK IF R AND L VARYING WITH W CAUSES THE DIFFERENCE\ ON THE LIFETIME
         lt_vs_freq = tau_from_waist(N0_list, R0_list, dvr, t_step, avg_no, tau,
                                     length, no_file, lt_vs_freq)
 
         if err:
             # TIGHTEST WAIST
-            dvr.VI *= dvr.V0_SI / dvr.kHz_2p
-            dvr.V0_SI = 76 * dvr.kHz_2p * hb  # trap depth for tightest waist
-            dvr.w = 8.61E-7 / a0  # tightest waist length
-            dvr.VI *= dvr.kHz_2p / dvr.V0_SI
+            dvr.V0 = 76 * dvr.kHz_2p  # trap depth for tightest waist
+            dvr.w = 8.61E-7  # tightest waist length
+            dvr.VIdV0 = dvr.VI / dvr.V0  # Update VI in unit of V0
 
             lt_err[0] = tau_from_waist(N0_list, R0_list, dvr, t_step, avg_no,
                                        tau, length, no_file, lt_err[0])
 
             # FATTEST WAIST
-            dvr.VI *= dvr.V0_SI / dvr.kHz_2p
-            dvr.V0_SI = 156 * dvr.kHz_2p * hb  # trap depth for fattest waist
-            dvr.w = 1.18E-6 / a0  # fattest waist length
-            dvr.VI *= dvr.kHz_2p / dvr.V0_SI
+            dvr.V0 = 156 * dvr.kHz_2p  # trap depth for fattest waist
+            dvr.w = 1.18E-6  # fattest waist length
+            dvr.VIdV0 = dvr.VI / dvr.V0  # Update VI in unit of V0
 
             lt_err[1] = tau_from_waist(N0_list, R0_list, dvr, t_step, avg_no,
                                        tau, length, no_file, lt_err[1])
@@ -370,13 +368,12 @@ def plot_lifetime(N0_list,
             #     ax2.set_title('FSS f=%dkHz w/ ' % sav[i, 0] + dvr.cvg_str)
             #     f2.savefig('{}d_{}_{}_fss.jpg'.format(dvr.dim, dvr.cvg,
             #                                            sav[i, 0]))
-        ax.errorbar(
-            sav[:, 0],
-            ext_lt[:, 0],
-            yerr=ext_lt[:, 1],
-            label='{}D {} with err'.format(dvr.dim, dvr.quantity)
-            # + ' $\Gamma$={:.2g}kHz'.format(dvr.VI * dvr.V0_SI / dvr.kHz_2p)
-        )
+        ax.errorbar(sav[:, 0],
+                    ext_lt[:, 0],
+                    yerr=ext_lt[:, 1],
+                    label='{}D {} with err'.format(dvr.dim, dvr.quantity)
+                    # + ' $\Gamma$={:.2g}kHz'.format(dvr.VI / dvr.kHz_2p)
+                    )
     for ni in range(len(N0_list)):
         dvr.update_N(N0_list[ni], N0_list[ni] * dvr.dx)
         if dvr.cvg == 'N':
@@ -400,7 +397,7 @@ def plot_lifetime(N0_list,
                 ax_label += 'sqr smooth'
             elif dvr.smooth_mode == 1:
                 ax_label += 'cos smooth'
-        # ax_label += '$\Gamma$={:.2g}kHz'.format(dvr.VI * dvr.V0_SI /
+        # ax_label += '$\Gamma$={:.2g}kHz'.format(dvr.VI /
         #                                         dvr.kHz_2p)
         # ax_label += ' '
         # ax_label += 'L={:.2g}w'.format(dvr.L)
@@ -670,8 +667,7 @@ def plot_wavefunction(N_list, R0_list, dvr: plot, length=1):
             ax.axvline(x=-1, color='w')
             ax.axvline(x=1, color='w')
             gs = sf[1].subplots()
-            ab_str = '$\Gamma$={:.2f}kHz'.format(dvr.VI * dvr.V0_SI /
-                                                 dvr.kHz_2p)
+            ab_str = '$\Gamma$={:.2f}kHz'.format(dvr.VI / dvr.kHz_2p)
             final_str = 'f={:.3f}kHz '.format(
                 dvr.freq) + dvr.cvg_str + ' ' + ab_str
             ax.set_title('{}D {} GS probability @ \n\
@@ -689,4 +685,4 @@ def plot_wavefunction(N_list, R0_list, dvr: plot, length=1):
             gs.set_title('{}D {} probabilities @ '.format(dvr.dim, dvr.model) +
                          dvr.cvg_str + ' ' + ab_str)
         plt.savefig('{}d_wavefunc_{:.2f}_{:.2f}.jpg'.format(
-            dvr.dim, dvr.freq, dvr.VI * dvr.V0_SI / dvr.kHz_2p))
+            dvr.dim, dvr.freq, dvr.VI / dvr.kHz_2p))

src/wannier.py
@@ -24,7 +24,7 @@ class Wannier(DVR):
         self.lattice = lattice.copy()
         self.graph, self.links = lattice_graph(lattice)
         if self.model == 'Gaussian':
-            self.lc = np.array(lc) * 1E-9 / (a0 * self.w)  # In unit of w
+            self.lc = np.array(lc) * 1E-9 / self.w  # In unit of w
         elif self.model == 'sho':
             self.lc = np.array(lc)
         self.Nsite = np.prod(self.lattice)
@@ -62,7 +62,7 @@ class Wannier(DVR):
             sparse: bool = True) -> None:
 
         self.N = N
-        self.scatt_len = ascatt
+        self.scatt_len = ascatt * a0
         self.dim = dim
         self.bands = band
         n = np.zeros(3, dtype=int)
@@ -321,7 +321,7 @@ def singleband_optimization(dvr: Wannier, E, W, parity):
 
     A = U.conj().T @ (
         E[:, None] *
-        U) * dvr.V0_SI / dvr.kHz_2p  # TB parameter matrix, in unit of kHz
+        U) * dvr.V0 / dvr.kHz_2p  # TB parameter matrix, in unit of kHz
     return A, U
 
 
@@ -330,7 +330,7 @@ def lattice_order(dvr: Wannier, W, U, p):
     shift = np.zeros((dvr.Nsite, dim))
     for i in range(dvr.Nsite):
         shift[i, :2] = dvr.graph[i] * dvr.lc
-        # Slight offset to avoid zero values on z=0 in odd sector
+        # Small offset to avoid zero values on z=0 in odd sector
         shift[i, 2] = 0.25
     x = [[[shift[i, d]] for d in range(dim)] for i in range(dvr.Nsite)]
     center_val = np.zeros((dvr.Nsite, dvr.Nsite))
@@ -364,23 +364,23 @@ def interaction(dvr: Wannier, U, W, parity: np.ndarray):
 
 def singleband_interaction(dvr: Wannier, Ui, Uj, Wi, Wj, pi: np.ndarray,
                            pj: np.ndarray):
-    u = 4 * np.pi * hb**2 * dvr.scatt_len * Eha/ (
-        dvr.m * dvr.kHz_2p * hb * dvr.w**dvr.dim)  # Unit to kHz
-    # Construct integral of 2-body eigenstates, due to basis quadrature it is reduced to sum 'ijk,ijk,ijk,ijk'
-    integrl = np.zeros(dvr.Nsite * np.ones(4, dtype=int))
-    intgrl_mat(dvr, Wi, Wj, pi, pj, integrl)  # np.ndarray is global variable
-    # Bands are degenerate, only differed by spin index, so they share the same set of Wannier functions
-    Uint = contract('ia,jb,kc,ld,ijkl->abcd', Ui.conj(), Uj.conj(), Uj, Ui,
-                    integrl)
-    Uint_onsite = np.zeros(dvr.Nsite)
-    for i in range(dvr.Nsite):
-        if dvr.model == 'sho':
-            print(
-                f'Test with analytic calculation on {i + 1}-th site',
-                np.real(Uint[i, i, i, i]) * (np.sqrt(2 * np.pi))**dvr.dim *
-                np.prod(dvr.hl))
-        Uint_onsite[i] = u * np.real(Uint[i, i, i, i])
-    return Uint_onsite
+    u = 4 * np.pi * dvr.hb * dvr.scatt_len / (
+        dvr.m * dvr.kHz_2p * dvr.w**dvr.dim)  # Unit to kHz
+    # # Construct integral of 2-body eigenstates, due to basis quadrature it is reduced to sum 'ijk,ijk,ijk,ijk'
+    # integrl = np.zeros(dvr.Nsite * np.ones(4, dtype=int))
+    # intgrl_mat(dvr, Wi, Wj, pi, pj, integrl)  # np.ndarray is global variable
+    # # Bands are degenerate, only differed by spin index, so they share the same set of Wannier functions
+    # Uint = contract('ia,jb,kc,ld,ijkl->abcd', Ui.conj(), Uj.conj(), Uj, Ui,
+    #                 integrl)
+    # Uint_onsite = np.zeros(dvr.Nsite)
+    # for i in range(dvr.Nsite):
+    #     if dvr.model == 'sho':
+    #         print(
+    #             f'Test with analytic calculation on {i + 1}-th site',
+    #             np.real(Uint[i, i, i, i]) * (np.sqrt(2 * np.pi))**dvr.dim *
+    #             np.prod(dvr.hl))
+    #     Uint_onsite[i] = u * np.real(Uint[i, i, i, i])
+    # return Uint_onsite
 
     x = []
     dx = []
@@ -407,7 +407,7 @@ def wannier_func(dvr, W, U, p, x: Iterable) -> np.ndarray:
     V = np.array([]).reshape(len(x[0]), len(x[1]), len(x[2]), 0)
     for i in range(p.shape[0]):
         V = np.append(V,
-                      psi(dvr.n, dvr.dx, W[i], *x, p[i, :])[..., None],
+                      psi(dvr.n, dvr.dx, W[i], *x, p[i, :]),
                       axis=dim)
         # print(f'{i+1}-th Wannier function finished.')
     return V @ U