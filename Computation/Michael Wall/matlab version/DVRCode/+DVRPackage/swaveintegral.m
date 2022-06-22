5295cf1088a5b6e918912afa092d5adaf62a9b0c
nottforestfc <weihaotian776@gmail.com>
2022-06-22 18:05:37 -0500

    Update release

--- Computation/Michael Wall/matlab version/DVRCode/+DVRPackage/swaveintegral.m
+++
@@ -1,7 +0,0 @@
-function y = swaveintegral(psi1, psi2, dx, dy, dz)
-    %Obtain the s-wave pseudopotential integral \int dr |\psi_1|^2 |\psi_2|^2 in units of the volume element [dx*dy*dz].
-    %As we integrate over the full domain, DVR quadrature (trapezoid rule) is exponentially convergent.
-    %Multiply by InteractionFromLengthScale to get the interaction matrix element in energy units
-    t1 = psi1.^2; t2 = psi2.^2;
-    y = sum(sum(sum(t1 .* t2))) * dx * dy * dz;
-end
