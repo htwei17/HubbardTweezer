061b42404961a0974d0540a36cf28a7d0574584e
nottforestfc <weihaotian776@gmail.com>
2022-06-24 17:07:55 -0500

    add src/tools

---
+++ Computation/src/tools/operator_to_full.py
@@ -0,0 +1,12 @@
+from typing import Callable
+import numpy as np
+import numpy.linalg as la
+
+
+def operator_to_full(applyH: Callable[[np.ndarray], np.ndarray],
+                         N: int) -> np.ndarray:
+    identity = np.eye(N)
+    Hmat = np.zeros((N, N))
+    for i in range(N):
+        Hmat[:, i] = applyH(identity[:, i])
+    return Hmat
