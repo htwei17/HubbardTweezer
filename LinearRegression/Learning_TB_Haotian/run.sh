8ab576b8323798e7d2e92033567e854c35b365e3
htwei17 <weihaotian776@gmail.com>
2023-09-05 11:47:06 -0500

    Update analysis on Wannierization

--- LinearRegression/Learning_TB_Haotian/run.sh
+++ LinearRegression/Learning_TB_Haotian/run.sh
@@ -1,1 +1,1 @@
-bash cluster/job.sh -l 4 -ly 4 -w None -e neq -r True -sy False -a 1-1000
+bash cluster/job.sh -l 4 -ly 4 -w None -e neq -r True -sy False -a 1001-2000
--- src/HubbardTweezer/Hubbard/eqinit.py
+++ src/HubbardTweezer/Hubbard/eqinit.py
@@ -7,7 +7,7 @@ from .lattice import Lattice
 
 dmin = 1.4 # Minimum trap center spacing in unit of wx
 # 1.4 wx is roughtly -0.75V0 barrier height
-dv = 0.05 # 5% fluctuation
+dv = 0.02 # 2% fluctuation
 
 def init_V0(Voff: np.ndarray, lattice: Lattice, nobounds: bool = False):
     v01 = symm_fold(lattice.reflect, Voff)
