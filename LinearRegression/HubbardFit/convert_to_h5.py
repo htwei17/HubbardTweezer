74aca52b1d48db33802f0ff62dc666f332d229c9
htwei17 <weihaotian776@gmail.com>
2024-01-26 12:01:46 -0600

    [Config] data analysis scripts

--- LinearRegression/HubbardFit/convert_to_h5.py
+++ LinearRegression/HubbardFit/convert_to_h5.py
@@ -3,12 +3,13 @@ import numpy as np
 import h5py
 import glob
 
-path = "/Users/nottforestfc/Library/CloudStorage/OneDrive-RiceUniversity/Documents/Research/Hubbard Tweezer Parameters/output/LinearRegression/samples_new/"
+path = "/Users/nottforestfc/Library/CloudStorage/OneDrive-RiceUniversity/Documents/Research/Hubbard Tweezer Parameters/output/LinearRegression/sample_finished/"
 keys = [
     "V_offset",
+    "V_tot",
     "trap_centers",
     "t_ij",
-    # "V_i", # STORED IN t_ij DIAGONALS
+    "V_i",
     "U_i",
     "wf_centers",
     "wf_cost",
@@ -17,7 +18,7 @@ keys = [
 ]
 dat = {k: [] for k in keys}
 
-for idx in range(3001):
+for idx in range(3003):
     filename = path + f"3D_4x4_square_None_neq_{idx}.ini"
     print(filename)
     report = rep.get_report(filename)
@@ -25,8 +26,9 @@ for idx in range(3001):
     dat["V_offset"].append(rep.a(report, "Trap_Adjustments", "V_offset"))
     dat["trap_centers"].append(rep.a(report, "Trap_Adjustments", "trap_centers"))
     tij = rep.a(report, "Singleband_Parameters", "t_ij")
-    tij += np.diag(rep.a(report, "Singleband_Parameters", "V_i"))
+    # tij += np.diag(rep.a(report, "Singleband_Parameters", "V_i"))
     dat["t_ij"].append(tij)
+    dat["V_i"].append(rep.a(report, "Singleband_Parameters", "V_i"))
     dat["U_i"].append(rep.a(report, "Singleband_Parameters", "U_i"))
     dat["wf_centers"].append(rep.a(report, "Singleband_Parameters", "wf_centers"))
     dat["wf_cost"].append(rep.a(report, "Singleband_Parameters", "wf_cost"))
@@ -35,8 +37,46 @@ for idx in range(3001):
     )
     dat["dir"].append(idx)
 
-output = "LinearRegression/Learning_TB_Haotian/alldata.hdf5"
+wxy = np.ones(2)
+width = 4
+height = 4
+N = width * height
+dim = 3
+
+
+def Vfun(x, y):
+    dxy = x**2 + y**2
+    V = -np.exp(-2 * dxy)
+    return V
+
+
+def tot_trap_depth(V0, trap_centers):
+    # tc = np.zeros((N, dim))
+    tc = np.zeros((N, 2))
+    vij = np.ones((N, N))
+    for i in range(N):
+        # tc[i, :] = np.append(trap_centers[i], 0)
+        tc[i, :] = trap_centers[i]
+        for j in range(i):
+            vij[i, j] = -Vfun(*(tc[i] - tc[j]))
+            vij[j, i] = vij[i, j]  # Potential is symmetric in distance
+    vtot = vij @ V0
+    return vtot
+
+
+dat["V_tot"] = np.array(list(map(tot_trap_depth, dat["V_offset"], dat["trap_centers"])))
+# vtot_rel = vtot - np.mean(vtot, axis=1)[:, None]
+
+output = "LinearRegression/HubbardFit/alldata.hdf5"
 with h5py.File(output, "w") as f:
     print(f"Writing to file {output}...")
     for k in keys:
         f[k] = np.asarray(dat[k])
+
+# Mask the data
+mask = np.min(np.asarray(dat["U_i"]), axis=1) > 0.8
+output = "LinearRegression/HubbardFit/alldata_masked.hdf5"
+with h5py.File(output, "w") as f:
+    print(f"Writing to file {output}...")
+    for k in dat.keys():
+        f[k] = np.asarray(dat[k])[mask]
