680c576b97aec800e7f231c2ad570fdedbca56a8
htwei17 <weihaotian776@gmail.com>
2024-05-15 18:12:38 -0500

    [Fix] run cluster job script

--- LinearRegression/HubbardFit/job.sh
+++ LinearRegression/HubbardFit/job.sh
@@ -247,20 +247,21 @@ echo "#!/bin/bash
 #SBATCH --mail-type=ALL
 #
 
-module getdefault
+module restore
 # conda initialization
-__conda_setup=\"\$('/opt/apps/software/Anaconda3/2022.10/bin/conda' 'shell.bash' 'hook' 2>/dev/null)\"
+__conda_setup=\"\$('/opt/apps/software/Anaconda3/2023.03-1/bin/conda' 'shell.bash' 'hook' 2>/dev/null)\"
 if [ \$? -eq 0 ]; then
     eval \"\$__conda_setup\"
 else
-    if [ -f \"/opt/apps/software/Anaconda3/2022.10/etc/profile.d/conda.sh\" ]; then
-        . \"/opt/apps/software/Anaconda3/2022.10/etc/profile.d/conda.sh\"
+    if [ -f \"/opt/apps/software/Anaconda3/2023.03-1/etc/profile.d/conda.sh\" ]; then
+        . \"/opt/apps/software/Anaconda3/2023.03-1/etc/profile.d/conda.sh\"
     else
-        export PATH=\"/opt/apps/software/Anaconda3/2022.10/bin:\$PATH\"
+        export PATH=\"/opt/apps/software/Anaconda3/2023.03-1/bin:\$PATH\"
     fi
 fi
 unset __conda_setup
-conda activate ~/env
+# <<< conda initialize <<<
+conda activate $HOME/env
 
 $NL_DEFINITION
 
