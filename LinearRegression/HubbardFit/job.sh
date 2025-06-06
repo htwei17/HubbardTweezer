d17009cc5656da16a647046f16c8661e1735e05b
htwei17 <weihaotian776@gmail.com>
2025-06-05 19:54:39 -0500

    [Update] 6x6 7x7 script

--- LinearRegression/HubbardFit/job.sh
+++ LinearRegression/HubbardFit/job.sh
@@ -13,8 +13,8 @@ RAND=False
 RAND_SAMPLE=False
 WAIST=xy
 STATUS=neq
-PARTITION=scavenge
-TIME="04:00:00"
+PARTITION=commons
+TIME="06:00:00"
 ARRAY="1-1"
 ARRAY_SUFFIX=""
 LOG=True
@@ -248,20 +248,27 @@ echo "#!/bin/bash
 #
 
 module restore
-# conda initialization
-__conda_setup=\"\$('/opt/apps/software/Anaconda3/2023.03-1/bin/conda' 'shell.bash' 'hook' 2>/dev/null)\"
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup=\"\$('/opt/apps/software/Miniforge3/24.1.2-0/bin/conda' 'shell.bash' 'hook' 2> /dev/null)\"
 if [ \$? -eq 0 ]; then
     eval \"\$__conda_setup\"
 else
-    if [ -f \"/opt/apps/software/Anaconda3/2023.03-1/etc/profile.d/conda.sh\" ]; then
-        . \"/opt/apps/software/Anaconda3/2023.03-1/etc/profile.d/conda.sh\"
+    if [ -f \"/opt/apps/software/Miniforge3/24.1.2-0/etc/profile.d/conda.sh\" ]; then
+        . \"/opt/apps/software/Miniforge3/24.1.2-0/etc/profile.d/conda.sh\"
     else
-        export PATH=\"/opt/apps/software/Anaconda3/2023.03-1/bin:\$PATH\"
+        export PATH=\"/opt/apps/software/Miniforge3/24.1.2-0/bin:\$PATH\"
     fi
 fi
 unset __conda_setup
+
+if [ -f \"/opt/apps/software/Miniforge3/24.1.2-0/etc/profile.d/mamba.sh\" ]; then
+    . \"/opt/apps/software/Miniforge3/24.1.2-0/etc/profile.d/mamba.sh\"
+fi
 # <<< conda initialize <<<
-conda activate $HOME/env
+
+# conda activate
+mamba activate $HOME/env
 
 $NL_DEFINITION
 
